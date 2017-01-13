d22f5ae6b0bd93228078728bd935866543d9ee9d
kudo183 <kudo183@gmail.com>
2017-01-13 17:15:44 +0700

    - add order option, change KhachRieng default value to false.

---
+++ Client/Client/ViewModel/RKhachHangViewModel.part.cs
@@ -0,0 +1,22 @@
+﻿using Client.Abstraction;
+using DTO;
+
+namespace Client.ViewModel
+{
+    public partial class RKhachHangViewModel : BaseViewModel<RKhachHangDto>
+    {
+        partial void InitFilterPartial()
+        {
+            OrderOptions.Add(new QueryBuilder.OrderByExpression.OrderOption()
+            {
+                PropertyPath = nameof(RKhachHangDto.TenKhachHang),
+                IsAscending = true
+            });
+        }
+
+        partial void ProcessNewAddedDtoPartial(RKhachHangDto dto)
+        {
+            dto.KhachRieng = false;
+        }
+    }
+}
