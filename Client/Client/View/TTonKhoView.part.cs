0f2b975e1d1cc1fb1b09daa671bc43bbd41b16c9
kudo183 <kudo183@gmail.com>
2017-02-15 18:49:37 +0700

    - add CopyTonKhoToClipboard in TTonKhoView.

--- Client/Client/View/TTonKhoView.part.cs
+++ Client/Client/View/TTonKhoView.part.cs
@@ -1,4 +1,6 @@
 ﻿using Client.Abstraction;
+using Client.ViewModel;
+using System.Windows.Controls;
 
 namespace Client.View
 {
@@ -10,6 +12,37 @@ namespace Client.View
 
             (GridView.Columns[4] as SimpleDataGrid.DataGridTextColumnExt).SetStyleAsRightAlignIntegerNumber();
             GridView.Columns[5].Visibility = System.Windows.Visibility.Collapsed;
+
+            var btnTonHangNhap = new Button()
+            {
+                Content = "Copy Ton Hang Nhap",
+                VerticalAlignment = System.Windows.VerticalAlignment.Top,
+                Margin = new System.Windows.Thickness(5)
+            };
+            btnTonHangNhap.Click += BtnTonHangNhap_Click;
+            GridView.CustomMenuItems.Add(btnTonHangNhap);
+
+
+            var btnTonHangNha = new Button()
+            {
+                Content = "Copy Ton Hang Nha",
+                VerticalAlignment = System.Windows.VerticalAlignment.Top,
+                Margin = new System.Windows.Thickness(5)
+            };
+            btnTonHangNha.Click += BtnTonHangNha_Click;
+            GridView.CustomMenuItems.Add(btnTonHangNha);
+        }
+
+        private void BtnTonHangNhap_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var vm = ViewModel as TTonKhoViewModel;
+            vm.CopyTonKhoToClipboard(false);
+        }
+
+        private void BtnTonHangNha_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var vm = ViewModel as TTonKhoViewModel;
+            vm.CopyTonKhoToClipboard(true);
         }
     }
 }
--- Client/Client/ViewModel/TTonKhoViewModel.part.cs
+++ Client/Client/ViewModel/TTonKhoViewModel.part.cs
@@ -1,5 +1,9 @@
 ﻿using Client.Abstraction;
 using DTO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
 
 namespace Client.ViewModel
 {
@@ -8,6 +12,64 @@ namespace Client.ViewModel
         partial void InitFilterPartial()
         {
             _NgayFilter.IsSorted = false;
+        }
+
+        public void CopyTonKhoToClipboard(bool? isHangNhaLam)
+        {
+            if (_NgayFilter.IsUsed == false || _MaKhoHangFilter.IsUsed == false)
+            {
+                return;
+            }
+
+            var qe = new QueryBuilder.QueryExpression();
+            if (isHangNhaLam.HasValue)
+            {
+                qe.AddWhereOption<QueryBuilder.WhereExpression.WhereOptionBool, bool>(
+                    QueryBuilder.WhereExpression.Equal,
+                    string.Format("{0}Navigation.{1}", nameof(TMatHangDto.MaLoai), nameof(RLoaiHangDto.HangNhaLam))
+                    , isHangNhaLam.Value);
+            }
+            var matHangs = ServiceLocator.Instance.GetInstance<IDataService<TMatHangDto>>().Get(qe).Items;
+
+            qe = new QueryBuilder.QueryExpression();
+
+            qe.AddWhereOption<QueryBuilder.WhereExpression.WhereOptionInt, int>(
+              QueryBuilder.WhereExpression.Equal, nameof(TTonKhoDto.MaKhoHang), (int)_MaKhoHangFilter.FilterValue);
+            qe.AddWhereOption<QueryBuilder.WhereExpression.WhereOptionDate, System.DateTime>(
+            QueryBuilder.WhereExpression.Equal, nameof(TTonKhoDto.Ngay), (System.DateTime)_NgayFilter.FilterValue);
+            qe.AddWhereOption<QueryBuilder.WhereExpression.WhereOptionIntList, List<int>>(
+                QueryBuilder.WhereExpression.In, nameof(TTonKhoDto.MaMatHang), matHangs.Select(p => p.Ma).ToList());
+
+            var tTonKhoes = ServiceLocator.Instance.GetInstance<IDataService<TTonKhoDto>>().Get(qe).Items;
+
+            var builder = new StringBuilder();
+
+            var matHangsDictionary = matHangs.ToDictionary(p => p.Ma, p => p);
+
+            var sortedList = new SortedList<string, string>();
+
+            var tongSoCoun = 0;
+            var tongSoKg = 0;
+            for (int i = 0; i < tTonKhoes.Count; i++)
+            {
+                var mh = matHangsDictionary[tTonKhoes[i].MaMatHang];
+                var tenMH = mh.TenMatHangDayDu;
+                var soKg = (tTonKhoes[i].SoLuong * mh.SoKy) / 10;
+                var soCoun = tTonKhoes[i].SoLuong;
+                var row = string.Format("{0}\t{1}\t{2}", tenMH, soCoun, soKg);
+                tongSoCoun += soCoun;
+                tongSoKg += soKg;
+                sortedList.Add(tenMH, row);
+            }
+
+            foreach (var item in sortedList)
+            {
+                builder.AppendLine(item.Value);
+            }
+
+            builder.Insert(0, string.Format("{0:dd/MM/yyyy}\t{1:N0} cuộn\t{2:N0} kg{3}"
+                , (System.DateTime)_NgayFilter.FilterValue, tongSoCoun, tongSoKg, System.Environment.NewLine));
+            Clipboard.SetText(builder.ToString());
         }
     }
 }
